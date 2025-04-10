110c97d829c6c9da2502618e3b4b8e4f819406bd
Napalm18 <gabrielsoaresdacosta18@gmail.com>
2025-04-10 16:45:00 -0300

    Add files via upload

---
+++ script_edited.js.js
@@ -0,0 +1,8 @@
+
+document.getElementById('analisar-perfil-button').addEventListener('click', function() {
+    alert('Funcionalidade de análise de perfil será implementada aqui.');
+});
+
+document.getElementById('contratar-button').addEventListener('click', function() {
+    alert('Funcionalidade de contratação será implementada aqui.');
+});
